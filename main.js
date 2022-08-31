5b2e62b59d9960b55c04b5052b5d4fd4ea065c67
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-08-30 19:38:07 -0500

    includes, case insensitive

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function updateCoffees(e) {
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
-        } else if (coffee.name.includes(selectName)) {
+        } else if (coffee.name.toLowerCase().includes(selectName.toLowerCase())) {
             filteredCoffees.push(coffee)
         }
     });
